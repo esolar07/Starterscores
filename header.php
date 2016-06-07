9917d23ef9b94d4d2ae4713930be586ecafd6cbf
esolar07 <esolar07@gmail.com>
2016-06-07 17:30:27 -0400

    adding styling to header. also add features for heading

--- header.php
+++ header.php
@@ -24,7 +24,26 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'starterscores' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+    <?php  // if conditional for header img ?>
+    <?php if ( get_header_image() ) : ?>
+	    <header id="masthead" class="site-header" sytle='background-image: url(<?php header_image(); ?>)' role="banner">
+    <?php } else { ?>
+        <header id="masthead" class="site-header" role="banner">
+    <?php } ?>
+
+        <?php // Display site icon or first letter as logo ?>
+        <div class="site-logo">
+            <?php $site_title = get_bloginfo('name'); ?>
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                <div class="screen-reader-text">
+                    <?php //printf(esc_html__('Go to the home page of )) ?>
+                </div>
+                <div class='site-firstletter' aria-hidden=\'true\'>
+                    <?php echo substr($site_title, 0,1); ?>
+                </div>
+             </a>
+        </div>
+
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
