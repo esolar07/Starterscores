7d3bbafb1fc4adf05a6568e8d1aec77c9ebb3121
Eddie Solar <e_solar@mgcorp.co>
2016-09-23 17:17:47 -0400

    working on featured image

--- functions.php
+++ functions.php
@@ -41,7 +41,9 @@ function starterscores_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
-
+	set_post_thumbnail_size(828, 360, true);
+	
+	
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'starterscores' ),
