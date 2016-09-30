684c0a0ad0f009a5cb5c68162114e7ae09869947
Eddie Solar <e_solar@mgcorp.co>
2016-09-30 16:58:11 -0400

    fixed issue where featured img not displaying

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,14 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if( has_post_thumbnail() ) { ?>
-				<figure class='featured-image'>
-					<?php the_post_thumbnail(); ?>
-				</figure>
-		<?php } 
-		?>
-		
+	<header class="entry-header">	
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
