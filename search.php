fef29806c726157999c50a5e3483ef4a7d440d91
Eddie Solar <e_solar@mgcorp.co>
2016-10-26 14:45:19 -0400

    made content template change to search page

--- search.php
+++ search.php
@@ -28,8 +28,12 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
-
+				
+				// underscores default 
+				//get_template_part( 'template-parts/content', 'search' );
+				
+				// adding starterscores template to match fronpage
+				get_template_part( 'template-parts/content', get_post_format() );
 			endwhile;
 
 			the_posts_navigation();
